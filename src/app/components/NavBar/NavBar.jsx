1a581dc71a4aaa65183ad8c75bfe70577f7317e2
MVCorreas <correasmv@gmail.com>
2024-07-02 20:22:45 -0300

    Added image to header

--- src/app/components/NavBar/NavBar.jsx
+++ src/app/components/NavBar/NavBar.jsx
@@ -2,6 +2,7 @@
 import Link from 'next/link';
 import React, { useState } from 'react';
 import styles from './NavBar.module.css'; 
+import Image from 'next/image'
 
 const navLinks = [
   {
@@ -27,9 +28,17 @@ const Navbar = () => {
 
   return (
     <nav className={styles.navbar}>
-      <div className={styles.logo}>
-        <h1>My name</h1>
-      </div>
+      <div className={styles.profile}>
+          <Image 
+            src="/Profile.jpeg" 
+            alt="profileImage"
+            width={50} 
+            height={50}
+            className={styles.profileImage}
+          />
+          <h1>My name</h1>
+        </div>
+
       <div className={styles.navLinks}>
         {navLinks.map((link, index) => (
           <h3 key={index} className={styles['nav-link']}>
