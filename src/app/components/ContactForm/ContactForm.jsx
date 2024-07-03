f7afbffb400c4cb5fcae528d0ba24d3a5b289f2b
MVCorreas <correasmv@gmail.com>
2024-07-03 19:56:14 -0300

    Styled image at Contact and About

--- src/app/components/ContactForm/ContactForm.jsx
+++ src/app/components/ContactForm/ContactForm.jsx
@@ -1,8 +1,9 @@
 "use client"
 import Image from 'next/image';
-import Link from 'next/link';
 import { useState } from 'react';
+import { useRouter } from 'next/navigation';
 import styles from './ContactForm.module.css';
+import Button from '../Button/Button';
 
 const ContactForm = () => {
   const [email, setEmail] = useState('');
@@ -13,12 +14,12 @@ const ContactForm = () => {
   const [messageError, setMessageError] = useState('');
   const [emailError, setEmailError] = useState('');
 
-  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/; // Basic email regex pattern
+
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    // Validate email format
     if (!emailRegex.test(email)) {
       setEmailError('Please enter a valid email address.');
       return;
@@ -26,7 +27,6 @@ const ContactForm = () => {
       setEmailError('');
     }
 
-    // Validate subject length
     if (subject.length > 50) {
       setSubjectError('Subject should not exceed 50 characters.');
       return;
@@ -34,7 +34,6 @@ const ContactForm = () => {
       setSubjectError('');
     }
 
-    // Validate message length
     if (message.length > 500) {
       setMessageError('Message should not exceed 500 characters.');
       return;
@@ -42,81 +41,95 @@ const ContactForm = () => {
       setMessageError('');
     }
 
-    // If all validations pass, proceed with form submission
     setShowThanksMessage(true);
   };
 
   return (
     <section id="contact" className={styles.section}>
-      <div className={styles.content}>
-        <h5 className={styles.title}>Let's Connect</h5>
-        <p className={styles.paragraph}>
-          I'm currently looking for new opportunities. You can contact me for further information.
-          I'll get back to you asap.
-        </p>
-      </div>
-     
-      <div className={styles.formContainer}>
-        {!showThanksMessage ? (
-          <form onSubmit={handleSubmit}>
-            <div className={styles.formGroup}>
-              <label htmlFor="email">Your email</label>
-              <input
-                name="email"
-                type="email"
-                id="email"
-                required
-                className={`${styles.inputField} ${emailError ? styles.inputError : ''}`}
-                placeholder="email@google.com"
-                value={email}
-                onChange={(e) => {
-                  setEmail(e.target.value);
-                }}
-              />
-              {emailError && <p className={styles.errorText}>{emailError}</p>}
+      <div className={styles.grid}>
+        <div className={styles.imageContainer}>
+          <Image 
+            src="/AboutImage.jpg" 
+            alt="aboutImage"
+            width={500} 
+            height={500}
+            className={styles.contactImage}
+          />
+        </div>
+        <div className={styles.content}>
+          <h5 className={styles.title}>Let's Connect</h5>
+          <p className={styles.description}>
+            I'm currently looking for new opportunities. You can contact me for further information.
+            I'll get back to you asap.
+          </p>
+          <div className={styles.formContainer}>
+          {!showThanksMessage ? (
+            <form onSubmit={handleSubmit}>
+              <div className={styles.formGroup}>
+                <label htmlFor="email">Your email</label>
+                <input
+                  name="email"
+                  type="email"
+                  id="email"
+                  required
+                  className={`${styles.inputField} ${emailError ? styles.inputError : ''}`}
+                  placeholder="email@google.com"
+                  value={email}
+                  onChange={(e) => {
+                    setEmail(e.target.value);
+                  }}
+                />
+                {emailError && <p className={styles.errorText}>{emailError}</p>}
+              </div>
+              <div className={styles.formGroup}>
+                <label htmlFor="subject">Subject</label>
+                <input
+                  name="subject"
+                  type="text"
+                  id="subject"
+                  required
+                  className={`${styles.inputField} ${subjectError ? styles.inputError : ''}`}
+                  placeholder="Just saying hi"
+                  value={subject}
+                  onChange={(e) => {
+                    setSubject(e.target.value);
+                  }}
+                />
+                {subjectError && <p className={styles.errorText}>{subjectError}</p>}
+              </div>
+              <div className={styles.formGroup}>
+                <label htmlFor="message">Message</label>
+                <textarea
+                  name="message"
+                  id="message"
+                  required
+                  className={`${styles.inputField} ${messageError ? styles.inputError : ''}`}
+                  placeholder="Let's talk about..."
+                  value={message}
+                  onChange={(e) => {
+                    setMessage(e.target.value);
+                  }}
+                />
+                {messageError && <p className={styles.errorText}>{messageError}</p>}
+              </div>
+              <Button 
+              type="submit" 
+              className={styles.submitButton}
+      
+              >
+                Send Message
+              </Button>
+            </form>
+          ) : (
+            <div className="text-center">
+              <p className="text-green-500 text-sm mt-2">
+                Thank you for contacting me!
+              </p>
             </div>
-            <div className={styles.formGroup}>
-              <label htmlFor="subject">Subject</label>
-              <input
-                name="subject"
-                type="text"
-                id="subject"
-                required
-                className={`${styles.inputField} ${subjectError ? styles.inputError : ''}`}
-                placeholder="Just saying hi"
-                value={subject}
-                onChange={(e) => {
-                  setSubject(e.target.value);
-                }}
-              />
-              {subjectError && <p className={styles.errorText}>{subjectError}</p>}
-            </div>
-            <div className={styles.formGroup}>
-              <label htmlFor="message">Message</label>
-              <textarea
-                name="message"
-                id="message"
-                required
-                className={`${styles.inputField} ${messageError ? styles.inputError : ''}`}
-                placeholder="Let's talk about..."
-                value={message}
-                onChange={(e) => {
-                  setMessage(e.target.value);
-                }}
-              />
-              {messageError && <p className={styles.errorText}>{messageError}</p>}
-            </div>
-            <button type="submit" className={styles.submitButton}>
-              Send Message
-            </button>
-          </form>
-        ) : (
-          <div className="text-center">
-            <p className="text-green-500 text-sm mt-2">
-              Thank you for contacting me!
-            </p>
-          </div>
-        )}
+          )}
+        </div>
+        </div>
+        
       </div>
     </section>
   );
