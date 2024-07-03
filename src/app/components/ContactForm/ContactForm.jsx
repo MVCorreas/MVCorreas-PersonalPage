a096b186b64318b55fd8ddcb8a49a404c6ed0b18
MVCorreas <correasmv@gmail.com>
2024-07-03 19:28:07 -0300

    Added form validation

--- src/app/components/ContactForm/ContactForm.jsx
+++ src/app/components/ContactForm/ContactForm.jsx
@@ -5,39 +5,45 @@ import { useState } from 'react';
 import styles from './ContactForm.module.css';
 
 const ContactForm = () => {
+  const [email, setEmail] = useState('');
   const [subject, setSubject] = useState('');
   const [message, setMessage] = useState('');
-  const [emailSubmitted, setEmailSubmitted] = useState(false);
+  const [showThanksMessage, setShowThanksMessage] = useState(false);
+  const [subjectError, setSubjectError] = useState('');
+  const [messageError, setMessageError] = useState('');
+  const [emailError, setEmailError] = useState('');
+
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/; // Basic email regex pattern
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const data = {
-      email: e.target.email.value,
-      subject: e.target.subject.value,
-      message: e.target.message.value,
-    };
-    const JSONdata = JSON.stringify(data);
-    const endpoint = '/api/sendEmail';
 
-    // Form the request for sending data to the server.
-    const options = {
-      // The method is POST because we are sending data.
-      method: 'POST',
-      // Tell the server we're sending JSON.
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      // Body of the request is the JSON data we created above.
-      body: JSONdata,
-    };
-
-    const response = await fetch(endpoint, options);
-    const resData = await response.json();
-
-    if (response.status === 200) {
-      console.log('Message sent.');
-      setEmailSubmitted(true);
+    // Validate email format
+    if (!emailRegex.test(email)) {
+      setEmailError('Please enter a valid email address.');
+      return;
+    } else {
+      setEmailError('');
     }
+
+    // Validate subject length
+    if (subject.length > 50) {
+      setSubjectError('Subject should not exceed 50 characters.');
+      return;
+    } else {
+      setSubjectError('');
+    }
+
+    // Validate message length
+    if (message.length > 500) {
+      setMessageError('Message should not exceed 500 characters.');
+      return;
+    } else {
+      setMessageError('');
+    }
+
+    // If all validations pass, proceed with form submission
+    setShowThanksMessage(true);
   };
 
   return (
@@ -48,68 +54,68 @@ const ContactForm = () => {
           I'm currently looking for new opportunities. You can contact me for further information.
           I'll get back to you asap.
         </p>
-        {/* <div className={styles.socials}>
-          <Link href="https://github.com/MVCorreas">
-           
-              <Image src="/github-icon.svg" alt="Github Icon" width={60} height={60} />
-           
-          </Link>
-          <Link href="https://www.linkedin.com/in/mar%C3%ADa-victoria-correas-148049b2/">
-           
-              <Image src="/linkedin-icon.svg" alt="Linkedin Icon" width={60} height={60} />
-           
-          </Link>
-        </div> */}
       </div>
-
+     
       <div className={styles.formContainer}>
-        <form onSubmit={handleSubmit}>
-          <div className={styles.formGroup}>
-            <label htmlFor="email">Your email</label>
-            <input
-              name="email"
-              type="email"
-              id="email"
-              required
-              className={styles.inputField}
-              placeholder="email@google.com"
-            />
+        {!showThanksMessage ? (
+          <form onSubmit={handleSubmit}>
+            <div className={styles.formGroup}>
+              <label htmlFor="email">Your email</label>
+              <input
+                name="email"
+                type="email"
+                id="email"
+                required
+                className={`${styles.inputField} ${emailError ? styles.inputError : ''}`}
+                placeholder="email@google.com"
+                value={email}
+                onChange={(e) => {
+                  setEmail(e.target.value);
+                }}
+              />
+              {emailError && <p className={styles.errorText}>{emailError}</p>}
+            </div>
+            <div className={styles.formGroup}>
+              <label htmlFor="subject">Subject</label>
+              <input
+                name="subject"
+                type="text"
+                id="subject"
+                required
+                className={`${styles.inputField} ${subjectError ? styles.inputError : ''}`}
+                placeholder="Just saying hi"
+                value={subject}
+                onChange={(e) => {
+                  setSubject(e.target.value);
+                }}
+              />
+              {subjectError && <p className={styles.errorText}>{subjectError}</p>}
+            </div>
+            <div className={styles.formGroup}>
+              <label htmlFor="message">Message</label>
+              <textarea
+                name="message"
+                id="message"
+                required
+                className={`${styles.inputField} ${messageError ? styles.inputError : ''}`}
+                placeholder="Let's talk about..."
+                value={message}
+                onChange={(e) => {
+                  setMessage(e.target.value);
+                }}
+              />
+              {messageError && <p className={styles.errorText}>{messageError}</p>}
+            </div>
+            <button type="submit" className={styles.submitButton}>
+              Send Message
+            </button>
+          </form>
+        ) : (
+          <div className="text-center">
+            <p className="text-green-500 text-sm mt-2">
+              Thank you for contacting me!
+            </p>
           </div>
-          <div className={styles.formGroup}>
-            <label htmlFor="subject">Subject</label>
-            <input
-              name="subject"
-              type="text"
-              id="subject"
-              required
-              className={styles.inputField}
-              placeholder="Just saying hi"
-              value={subject}
-              onChange={(e) => {
-                setSubject(e.target.value);
-              }}
-            />
-          </div>
-          <div className={styles.formGroup}>
-            <label htmlFor="message">Message</label>
-            <textarea
-              name="message"
-              id="message"
-              className={styles.inputField}
-              placeholder="Let's talk about..."
-              value={message}
-              onChange={(e) => {
-                setMessage(e.target.value);
-              }}
-            />
-          </div>
-          <button type="submit" className={styles.submitButton}>
-            Send Message
-          </button>
-        </form>
-
-        {emailSubmitted && (
-          <p className={styles.successMessage}>Thank you for contacting me!</p>
         )}
       </div>
     </section>
