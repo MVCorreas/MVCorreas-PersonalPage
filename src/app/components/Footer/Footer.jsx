236d0269de90978cc7069f018938f014497d5059
MVCorreas <correasmv@gmail.com>
2024-07-04 20:15:08 -0300

    some colour palette

--- src/app/components/Footer/Footer.jsx
+++ src/app/components/Footer/Footer.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import styles from "./Footer.module.css";
 import Image from "next/image";
 import Link from "next/link";
+import GithubIcon from "../reusableIcons/GithubIcon";
+import LinkedInIcon from "../reusableIcons/LinkedInIcon";
 
 const Footer = () => {
   return (
@@ -10,10 +12,10 @@ const Footer = () => {
         <span className={styles.footerTitle}>MARIA VICTORIA CORREAS PROFILE</span>
        <div className={styles.iconContainer}>
        <Link href="https://github.com/MVCorreas">
-            <Image src='/github-icon.svg' alt="Github Icon" width={50} height={50} className={styles.icon}/>
+            <GithubIcon color={'#b3074f'} width={50} height={50}/>
           </Link>
           <Link href="https://www.linkedin.com/in/mar%C3%ADa-victoria-correas-148049b2/"  className={styles.icon}>
-            <Image src='/linkedin-icon.svg' alt="Linkedin Icon"  width={50} height={50}/>
+            <LinkedInIcon color={'#b3074f'} width={50} height={50}/>
           </Link>
        </div>
        
