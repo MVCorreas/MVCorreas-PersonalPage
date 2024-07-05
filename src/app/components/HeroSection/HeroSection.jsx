3ae04ba3b140ee3ee99a3694f6e2a540552d0b0f
MVCorreas <correasmv@gmail.com>
2024-07-04 22:51:17 -0300

    Added loader

--- src/app/components/HeroSection/HeroSection.jsx
+++ src/app/components/HeroSection/HeroSection.jsx
@@ -1,12 +1,33 @@
-import styles from './HeroSection.module.css'
+"use client"
+import { useState } from 'react';
+import Image from 'next/image';
+import styles from './HeroSection.module.css';
 
 const HeroSection = () => {
+  const [imageLoaded, setImageLoaded] = useState(false);
 
-    return (
-        <section className={styles.section} id="hero">
-            <h1 className={styles.heroTitle}>A LITTLE ABOUT MARIA</h1>
-        </section>
-    )
-}
+  const handleImageLoad = () => {
+    setImageLoaded(true); 
+  };
 
-export default HeroSection
+  return (
+    <section className={styles.section} id="hero">
+      {!imageLoaded && <div className={styles.loader}></div>}
+      <div className={styles.heroImageWrapper}>
+        <Image
+          src="/BackgroundImage.jpg"
+          alt="Background image"
+          layout="fill"
+          objectFit="cover"
+          className={styles.heroImage}
+          priority={true} 
+          onLoad={handleImageLoad} 
+          style={{ opacity: imageLoaded ? 1 : 0 }} 
+        />
+      </div>
+      <h1 className={styles.heroTitle}>A LITTLE ABOUT MARIA</h1>
+    </section>
+  );
+};
+
+export default HeroSection;
